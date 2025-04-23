c84dfeb56825670c76b13e94b5518c9bc3def770
DiyanAmin <pakuptl3112@gmail.com>
2025-04-23 11:03:26 +0530

    1commit

---
+++ print.py
@@ -0,0 +1,7 @@
+# this print statement displays the message hello world
+print("hello world")
+# This is about myself
+print(" My name is Diyan")
+print("I am in grade 6 and I am 11 years old.")
+# Perfoming and printing a simple calculation
+print("The answer of 12+12 is",12+12)
