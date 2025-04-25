e4c7a854149c4cbb9fddeb5274296987d4370a9c
DiyanAmin <pakuptl3112@gmail.com>
2025-04-25 18:48:31 +0530

    1st

---
+++ ram_congratulations.py
@@ -0,0 +1,9 @@
+#Converting "congratulations" to "CONGRATULATIONS using input"
+print('There are 2 things')
+fixing = str(input('str Input word: ')) #-> Enter str Input in lowercase
+print('Original:\n',fixing) #-> Displays original string input
+print('Uppercased:\n',fixing.upper()) #->Makes original string input upper case
+#For congratulations
+congrat=str(input('Enter "congratulations": '))
+print('Original:\n',congrat)
+print('Uppercase:\n',congrat.upper())
