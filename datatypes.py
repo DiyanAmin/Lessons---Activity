d472e8bfe30aece6c241574502b94c3c450185fd
DiyanAmin <pakuptl3112@gmail.com>
2025-04-25 17:41:18 +0530

    commit1

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+#Data Types
+date = 15
+print('Birthday',date)
+print('Type of data birthday is',type(date))
+#Float
+pi = 3.14
+print('The value of pi is',pi)
+print('The type of data pi is',type(pi))
+#String
+name = 'Diyan'
+print('My name is',name)
+print('The type of vriable my name is',type(name))
+#Boolean
+human = True
+print('Is he human?',human)
+print('The type he is',type(human))
+#Type Casting
+date = (float(date))
+print('Changed from int to float',type(date))
+pi = (str(pi))
+print('Changed from float to str',type(pi))
