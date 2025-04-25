a44b8dbc20056233aae0f6cfc02588a511a50a58
DiyanAmin <pakuptl3112@gmail.com>
2025-04-25 18:22:09 +0530

    2commit

--- revstr.py
+++ revstr.py
@@ -3,3 +3,8 @@ text = str(input('Enter string: '))
 print('Original string',text)
 revText= text[::-1]
 print('Reverse of string input', revText)
+g = str(input())
+print('Your 1st sentence',g)
+h = str(input())
+print('Your 2nd sentence',h)
+print(g+h)
