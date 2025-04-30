5c089485b6a0fd3c3a7794d8ab59204486c86aca
DiyanAmin <pakuptl3112@gmail.com>
2025-04-30 13:18:36 +0530

    commit1

---
+++ conditionalstatements.py
@@ -0,0 +1,5 @@
+#Conditional Statements
+#Positive or negative number
+num=int(input('Enter a number: '))
+if num>0:
+    print('Positive Number')
---
+++ rs.py
@@ -0,0 +1,7 @@
+#Checking if temperature is appropriate for wearing light clothes
+print('Temperatures suitable for wearing light clothes:\n24 degrees celcius and above')
+temp=int(input('Enter temperature in degrees celcius: '))
+if temp>24:
+    print('Temperature is suitable for light clothes.')
+else:
+    print('Temperature is not suitable for light clothes')
