904dbe9e21196eb46f00e0ddb41a600e8ea51145
DiyanAmin <pakuptl3112@gmail.com>
2025-05-06 19:29:03 +0530

    commit1

--- asciivalue.py
+++ asciivalue.py
@@ -1,4 +1,5 @@
 #Checking ascii value of different alphabets
 askiinput=str(input('Enter alphabet: '))
-aski_val=ord(askiinput)
+askiiinputletter1=askiinput[-1]
+aski_val=ord(askiiinputletter1)
 print('Ascii value:\n',aski_val)
