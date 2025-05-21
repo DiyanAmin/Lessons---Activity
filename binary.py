1fb34c634ff8a11b8ceaf15a81d5dedd1fb2ec6e
DiyanAmin <pakuptl3112@gmail.com>
2025-05-21 11:13:36 +0530

    commit1_2

---
+++ binary.py
@@ -0,0 +1,8 @@
+#Converting decimal  umber to binary number
+num=float(input('Enter a decimal number to convert it to binary: '))
+binary_val=''
+while num>0:
+ rem=num%2
+ binary_val=str(rem)+binary_val
+ num=num//2
+print('Binary Number:\n',binary_val)
---
+++ noofdigiys.py
@@ -0,0 +1,8 @@
+#Calculating number of digits in given number
+# num=int(input('Enter a number: '))
+# digits=0
+# while num>=1:
+#     num-1
+#     digits+1
+# print('Number of digits:\n',digits)
+print("hi")
